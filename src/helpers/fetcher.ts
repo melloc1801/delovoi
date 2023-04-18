13e61f3cb1a82e73aaa90b86e384f5f0b28ae8eb
melloc1801 <dmitry.tomahawk@yandex.ru>
2023-04-18 20:06:00 +0500

    feat(:bux:): fix unauthorized

--- src/helpers/fetcher.ts
+++ src/helpers/fetcher.ts
@@ -1,5 +1,6 @@
 import axios, { type AxiosRequestConfig, type AxiosResponse } from 'axios';
 import { appConfig } from '../app-config';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 const instance = axios.create({
   baseURL: 'https://api.delovoi.me/api/',
@@ -9,6 +10,22 @@ const instance = axios.create({
     'Api-Front-Token': appConfig.apiKey,
   },
 });
+
+const ls = useLocalStorage();
+instance.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  (error: unknown) => {
+    if (error instanceof Error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 401) {
+          ls.removeToken();
+          location.reload();
+        }
+      }
+    }
+    throw error;
+  }
+);
 
 export default async function fetcher<JSON = unknown>(
   input: AxiosRequestConfig
